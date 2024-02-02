535aaa2a6f0a09ccc1604fdb227fbe64174a2d67
Tofunmi Adewuyi <tiadewuyi@gmail.com>
2024-02-02 19:15:46 +0100

    completed auth with clerk and convex

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://supreme-seal-30.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { ClerkProvider, useAuth } from '@clerk/nextjs';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
+import { 
+    AuthLoading,
+    Authenticated,
+    ConvexReactClient
+ } from 'convex/react';
+import { Loading } from '@/components/auth/loading';
+
+ interface ConvexProviderProps {
+    children: React.ReactNode;
+ };
+
+ const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+ const convex = new ConvexReactClient(convexUrl);
+
+export const ConvexClientProvider = ({
+    children
+}: ConvexProviderProps) => {
+    return (
+        <ClerkProvider>
+            <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+                <Authenticated>
+                    {children}
+                </Authenticated>
+                <AuthLoading>
+                    <Loading/>
+                </AuthLoading>
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    );
+};
