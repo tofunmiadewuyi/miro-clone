9d02b6635c1812cbae669318a0de499e83e0174e
Tofunmi Adewuyi <tiadewuyi@gmail.com>
2024-02-10 23:25:30 +0100

    completed the participants bar

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+const COLORS = ["#dc2626", "#d97706", "#059669", "#7c3aed", "#db2777"];
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length]
 }
