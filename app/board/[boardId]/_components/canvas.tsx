4a9b20da28ddaf50a8564f97f324ad00d489d4e5
Tofunmi Adewuyi <tiadewuyi@gmail.com>
2024-02-11 00:15:42 +0100

    created types and enums for the toolbar implemented the active functionality for each tool completed the toolbar

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,23 +1,40 @@
 "use client"
 
+import { useState } from "react";
+
+import { CanvasMode, CanvasState } from "@/types/canvas";
+
 import { Info } from "./info"
 import { Participants } from "./participants"
 import { Toolbar } from "./toolbar"
 
-import { useSelf } from "@/liveblocks.config"
+import { useHistory, useCanUndo, useCanRedo } from "@/liveblocks.config"
 
 interface  CanvasProps {
     boardId: string
 }
 
 export const Canvas = ({boardId} : CanvasProps) => {
-    const info = useSelf((me) => me.info);
+    const [canvasState, setCanvasState] = useState<CanvasState>({
+        mode: CanvasMode.None
+    });
+
+    const history = useHistory();
+    const canUndo = useCanUndo();
+    const canRedo = useCanRedo();
 
     return(
         <main className="h-full w-full relative bg-neutral-100 touch-none">
             <Info boardId={boardId}/>
             <Participants/>
-            <Toolbar/>
+            <Toolbar
+                canvasState={canvasState}
+                setCanvasState={setCanvasState}
+                canRedo={canRedo}
+                canUndo={canUndo}
+                redo={history.redo}
+                undo={history.undo}
+            />
         </main>
     )
 }
